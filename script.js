558163538548069ae9d029ff70a795ffdade1422
guidobenigni <guidogeza@gmail.com>
2024-05-22 00:16:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const formData = new FormData(document.getElementById('registrationForm'));
 
         // Send form data to Google Form after successful payment
-        fetch('URL_OF_YOUR_GOOGLE_FORM', {
+        fetch('https://docs.google.com/forms/d/e/1FAIpQLSdq6QP5SwxxLuVOXmDTcqQgzViSwFu2xvhnb3rHK9Yt5Hp9wQ/formResponse??usp=pp_url', {
           method: 'POST',
           body: formData
         }).then(response => response.ok ? alert('Zahlung erfolgreich! Anmeldung abgeschlossen.') : alert('Fehler bei der Anmeldung. Bitte versuchen Sie es erneut.'));
