8ca96739b681a914923b73993e00383303ceb4e8
guidobenigni <guidogeza@gmail.com>
2024-06-06 01:10:04 +0200

    Update package.js

--- package.js
+++ package.js
@@ -1,1 +1,17 @@
-
+{
+  "name": "schwimmwettkampf-anmeldung",
+  "version": "1.0.0",
+  "description": "Backend für die Schwimmwettkampf-Anmeldung",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "author": "Dein Name",
+  "license": "MIT",
+  "dependencies": {
+    "express": "^4.18.2",
+    "body-parser": "^1.20.1",
+    "multer": "^1.4.5-lts.1",
+    "@google-cloud/storage": "^7.7.0"
+  }
+}
