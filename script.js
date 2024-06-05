3cec0838aa7ccb12356e6904291aecd8b46d9bdc
guidobenigni <guidogeza@gmail.com>
2024-06-05 22:34:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,73 @@
-function uploadFile() {
-  var form = document.getElementById('myForm');
-  var registrationForm = form.registrationForm.files[0];
-  var photo = form.photo.files[0];
+async function uploadFile(event) {
+  event.preventDefault();
+  const form = document.getElementById('myForm');
+  const formData = new FormData(form);
 
-  if (registrationForm.size > 1048576 || photo.size > 1048576) {
+  const firstName = formData.get('firstName');
+  const lastName = formData.get('lastName');
+  const registrationFormFile = formData.get('registrationForm');
+  const photoFile = formData.get('photo');
+
+  if (registrationFormFile.size > 1048576 || photoFile.size > 1048576) {
     alert("Dateien dürfen nicht größer als 1 MB sein.");
     return;
   }
 
-  var img = new Image();
-  img.onload = function() {
+  const photo = new Image();
+  photo.onload = async function() {
     if (this.width < 425 || this.height < 566) {
       alert("Das Passfoto muss mindestens 425x566 Pixel groß sein.");
       return;
     } else {
-      var formData = new FormData(form);
-      google.script.run.withSuccessHandler(updateOutput).uploadFile(formData);
+      const reader1 = new FileReader();
+      const reader2 = new FileReader();
+
+      reader1.readAsDataURL(registrationFormFile);
+      reader2.readAsDataURL(photoFile);
+
+      reader1.onload = async function() {
+        reader2.onload = async function() {
+          const registrationFormBase64 = reader1.result.split(',')[1];
+          const photoBase64 = reader2.result.split(',')[1];
+
+          const data = {
+            firstName: firstName,
+            lastName: lastName,
+            registrationForm: registrationFormBase64,
+            registrationFormMimeType: registrationFormFile.type,
+            photo: photoBase64,
+            photoMimeType: photoFile.type
+          };
+
+          try {
+            const response = await fetch('https://script.google.com/macros/s/AKfycbyGjlpGBavjLc-oRg0Fg-Af5fYcPxf85oP7R7CmHzpTi3vtYGIlTHLsWyuViX3WPA9P/exec', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(data)
+            });
+
+            const result = await response.json();
+            if (result.status === 'success') {
+              document.getElementById('output').innerHTML = `
+                Dateien hochgeladen: <br>
+                <a href="${result.registrationFileUrl}">Meldezettel</a><br>
+                <a href="${result.photoFileUrl}">Passfoto</a>
+              `;
+            } else {
+              alert('Fehler: ' + result.message);
+            }
+          } catch (error) {
+            alert('Fehler: ' + error.toString());
+          }
+        };
+      };
     }
   };
-  img.src = URL.createObjectURL(photo);
+  photo.src = URL.createObjectURL(photoFile);
 }
 
-function updateOutput(output) {
-  document.getElementById('output').innerHTML = output;
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  alert("Formular erfolgreich eingereicht!");
-  // Hier können Sie zusätzliche Aktionen beim Abschluss des Formulars ausführen
-}
 
 
 
